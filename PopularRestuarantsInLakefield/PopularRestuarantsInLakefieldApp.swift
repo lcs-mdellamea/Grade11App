3c3523d116973a0d8dd2214feb2e254374d79168
Madison Dellamea <mdellamea@lcs.on.ca>
2021-11-29 14:26:30 -0500

    Initial Commit

---
+++ PopularRestuarantsInLakefield/PopularRestuarantsInLakefieldApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopularRestuarantsInLakefieldApp.swift
+//  PopularRestuarantsInLakefield
+//
+//  Created by Madison Dellamea on 11/29/21.
+//
+
+import SwiftUI
+
+@main
+struct PopularRestuarantsInLakefieldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
